0d21fe02488785078ffc9ac869828b3baaac123e
jae0107 <ghjgjh0107@gmail.com>
2021-09-08 02:17:50 +0930

    structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a Covid 19 Tracker!!!</h1>
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app__dropdown" >
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
