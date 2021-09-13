b196552ca629a68e96785199ae38a96be1bc3f3a
jae0107 <ghjgjh0107@gmail.com>
2021-09-13 23:22:55 +0930

    import disease.sh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) =>  {
+          const countries = data.map((country) => ({
+            name: country.country, // Full name of countriåes
+            value: country.countryInfo.iso2 // UK, US, ID, KR etc
+          }));
+
+          setCountries(countries);
+        })
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__header">
         <h1>Covid-19 Tracker</h1>
         <FormControl className="app__dropdown" >
           <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option two</MenuItem>
-            <MenuItem value="worldwide">Option three</MenuItem>
-            <MenuItem value="worldwide">Option four</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
           </Select>
         </FormControl>
       </div>
