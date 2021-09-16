169c1c2f6dd004a0369f9bb7d9ba51647094864a
jae0107 <ghjgjh0107@gmail.com>
2021-09-16 20:38:30 +0930

    info box

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -5,13 +5,10 @@ const InfoBox = ({ title, cases, total }) => {
     return (
         <Card className="infoBox">
             <CardContent>
-                {/* Title */}
                 <Typography className="infoBox__title" color="textSecondary">
                     {title}
                 </Typography>
-                {/* +120k Number of cases */}
                 <h2 className="infoBox__cases">{cases}</h2>
-                {/* 1.2M Total */}
                 <Typography className="infoBox__total" color="textSecondary">
                     {total} Total
                 </Typography>
