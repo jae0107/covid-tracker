ca4b766bbf412dc04fdeb0e9b89ae0fc0ae15c3f
jae0107 <ghjgjh0107@gmail.com>
2021-09-18 14:20:38 +0930

    bubbles

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -37,6 +38,7 @@ function App() {
           const sortedData = sortData(data);
           setTableData(sortedData);
           setCountries(countries);
+          setMapCountries(data);
         })
     };
 
@@ -56,8 +58,15 @@ function App() {
         .then(data => {
           setCountry(countryCode);
           setCountryInfo(data); // All of the data from the country response
-          setMapCenter({ lat: data.countryInfo.lat, lng: data.countryInfo.long });
-          setMapZoom(4);
+
+          if(countryCode === "worldwide"){
+            setMapCenter({ lat: 34.80746, lng: -40.4796 });
+            setMapZoom(3);
+            
+          } else {
+            setMapCenter({ lat: data.countryInfo.lat, lng: data.countryInfo.long });
+            setMapZoom(4);
+          }
         })
   };
 
@@ -82,7 +91,7 @@ function App() {
           <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
 
-        <Map center={mapCenter} zoom={mapZoom}/>
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom}/>
       </div>
       <Card className="app__right">
         <CardContent>
--- src/util.js
+++ src/util.js
@@ -1,3 +1,21 @@
+import numeral from 'numeral';
+import { Circle, Popup } from 'react-leaflet';
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        multiplier: 800,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        multiplier: 1200,
+    },
+    deaths: {
+        hex: "#fb4443",
+        multiplier: 2000,
+    },
+};
+
 export const sortData = (data) => {
     let sortedData = [...data];
 
@@ -10,4 +28,37 @@ export const sortData = (data) => {
     });
     
     return sortedData;
-  };
+};
+
+//Draw circles on the map with interactive tooltop
+export const showDataOnMap = (data, casesType = "cases") => (
+    data.map(country => (
+        <Circle 
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
+            radius={
+                (Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier)/6
+            }
+        >
+            <Popup>
+                <div className="info-container">
+                    <div className="info-flag" style={{ backgroundImage: `url(${country.countryInfo.flag})` }}></div>
+                    <div className="info-name">
+                        {country.country}
+                    </div>
+                    <div className="info-confirmed">
+                        Cases: {numeral(country.cases).format("0,0")}
+                    </div>
+                    <div className="info-recovered">
+                        Recovered: {numeral(country.recovered).format("0,0")}
+                    </div>
+                    <div className="info-deaths">
+                        Deaths: {numeral(country.deaths).format("0,0")}
+                    </div>
+                </div>
+            </Popup>
+        </Circle>
+    ))
+);
